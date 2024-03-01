7f2205c276898b06ae949f30a07ee652b328b626
Orekhov Maksim <timberwolf.max@gmail.com>
2024-03-01 13:57:27 +0200

    QrcodeStream added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
 import { Alert, MainButton } from 'vue-tg'
+import { ref } from 'vue'
+import { QrcodeStream } from 'vue-qrcode-reader'
 
 function handleAlertClose() {
     // ...
@@ -9,6 +11,36 @@ function handleAlertClose() {
 function handleBackButton() {
     // ...
 }
+
+const result = ref('')
+const scanning = ref(false)
+
+function startScanning() {
+    scanning.value = true
+}
+
+function onDecode(content) {
+    result.value = content
+    scanning.value = false
+}
+
+function onInit(promise) {
+    promise.then(() => {
+        console.log('Ready to scan!')
+    }, error => {
+        if (error.name === 'NotAllowedError') {
+            // пользователь отказался предоставить доступ к камере
+        } else if (error.name === 'NotFoundError') {
+            // нет устройства для захвата медиа
+        } else if (error.name === 'NotReadableError') {
+            // невозможно получить поток медиа
+        } else if (error.name === 'OverconstrainedError') {
+            // ограничения не могут быть удовлетворены
+        } else {
+            // другая ошибка
+        }
+    })
+}
 </script>
 
 <template>
@@ -16,5 +48,8 @@ function handleBackButton() {
     <TheWelcome />
 <!--      <Alert message="Hello!" @close="handleAlertClose" />-->
       <MainButton @click="handleBackButton" />
+      <button @click="startScanning">Начать сканирование</button>
+      <qrcode-stream v-if="scanning" @decode="onDecode" @init="onInit"></qrcode-stream>
+      <p>{{ result }}</p>
   </main>
 </template>
